684646d69574305eb7cd807b28d1ac55cee2f79b
Md Sadman Chowdhury <61442059+SpicyCatGames@users.noreply.github.com>
2025-06-08 13:50:37 +0600

    initial

---
+++ Chapter3BasicProducerConsumer/ProducerConsumerRabbitMQ/Consumer/Receiver.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var connectionFactory = new ConnectionFactory() { HostName = "localhost", Port = 5672 };
+using var connection = await connectionFactory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+await channel.QueueDeclareAsync("BasicTest", false, false, false, null);
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+consumer.ReceivedAsync += async (model, ea) =>
+{
+    try
+    {
+        var body = ea.Body;
+        var message = Encoding.UTF8.GetString(body.Span);
+        Console.WriteLine($"Received message: {message}");
+
+        await channel.BasicAckAsync(ea.DeliveryTag, multiple: false);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error processing message: {ex.Message}");
+
+        await channel.BasicNackAsync(ea.DeliveryTag, multiple: false, requeue: true);
+    }
+};
+
+await channel.BasicConsumeAsync("BasicTest", autoAck: false, consumer);
+Console.ReadKey();
---
+++ Chapter3BasicProducerConsumer/ProducerConsumerRabbitMQ/Producer/Sender.cs
@@ -0,0 +1,15 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost", Port = 5672 };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+await channel.QueueDeclareAsync("BasicTest", false, false, false, null);
+
+string message = "Getting  started with .Net RabbitMQ";
+var body = Encoding.UTF8.GetBytes(message);
+
+await channel.BasicPublishAsync("", "BasicTest", body);
+Console.WriteLine($"Sent message {message}");
+
+Console.ReadKey();
